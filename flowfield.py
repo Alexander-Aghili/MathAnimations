427400ee3259fbe423f3fcec9ae2eb3ef6623d7f
Alexander Aghili <awaghili@ucsc.edu>
2023-11-24 22:03:45 -0800

    Flow field

--- flowfield.py
+++ flowfield.py
@@ -2,10 +2,19 @@ from manim import *
 
 class FlowField(Scene):
     def construct(self):
-        func = lambda pos: np.sin(pos[0]) * UR + np.cos(pos[1]) * LEFT + pos / 5
-        stream_lines = StreamLines(
-            func, x_range=[-3, 3, 0.2], y_range=[-2, 2, 0.2], padding=1
-        )
-
-        self.play(stream_lines.create())
+        func = lambda pos: np.sin(pos[0] / 2) * UR + np.cos(pos[1] / 2) * LEFT
+        vf = ArrowVectorField(func, x_range=[-7, 7, 1])
+        self.add(vf)
         self.wait()
+
+        length_func = lambda x: .5*x
+        vf2 = ArrowVectorField(func, x_range=[-7, 7, 1], length_func=length_func)
+        self.play(vf.animate.become(vf2))
+        self.wait()
+
+        func = lambda pos: pos / 2
+        vf3 = ArrowVectorField(func, x_range=[-7, 7, 1])
+        self.play(vf.animate.become(vf3))
+        self.wait()
+
+        
